1e3b497a2c642dc0ba40b6465d5854e6cb99ef5f
DivineDemon <supame123@gmail.com>
2023-07-02 18:22:12 +0500

    🚀

--- src/pages/ContactCard.vue
+++ src/pages/ContactCard.vue
@@ -17,9 +17,9 @@
       v-motion-pop-visible
       :delay="600"
     >
-      Although I'm not currently looking for any new opportunities, my inbox is
-      always open. Whether you have a question or just want to say hi, I'll try
-      my best to get back to you!
+      I'm currently looking for any new opportunities, my inbox is always open.
+      Whether you have a question or just want to say hi, I'll try my best to
+      get back to you!
     </p>
     <form
       ref="form"
